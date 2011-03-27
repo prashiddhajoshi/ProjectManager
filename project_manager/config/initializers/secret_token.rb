14996424e2a8d54e5462e095a7f7d68871d3d48a
unknown <prasiddha@.(none)>
2011-03-27 18:34:14 +0545

    project_manager initials

---
+++ project_manager/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectManager::Application.config.secret_token = 'ef8c4a263b7bbb5e04923ad9bddbb95b495cc45c3977a21f47db295b34ee8c8fcef721e7ae22aecf52d55755be352a326cd7952a4fdb9e09bb01580c912a25d1'
